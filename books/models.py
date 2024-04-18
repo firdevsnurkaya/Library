7a98555009bfa1fa9f40b5a58ef38753ebdfc1bf
Firdevs Nur Kaya <firdevsnurkaya@gmail.com>
2024-04-19 02:00:22 +0300

    slug eklendi

--- books/models.py
+++ books/models.py
@@ -1,57 +1,60 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, default=None)
     biography = models.TextField(max_length=1000, blank=True, null=True)
-
-
-    def __str__(self):
-        return self.name
-    # bir nesnenin herhangi bir attiributeu çağırılmadan nesneyi çağırdığınızda ne görülsün istiyorsanız, nesneyi ne şekilde 
-    #adlandırmak istiyorsanız, ne şekilde çağırmak istiyorsanız onu return etmek gerekiyor
-
-
-    class Meta: # ek bilgi eklemek için kullanılır
-        verbose_name = "Yazar"
-        verbose_name_plural = "Yazarlar"
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return self.name
     
-
-    class Meta: # ek bilgi eklemek için kullanılır
+    class Meta:
+        verbose_name = "Yazar"
+        verbose_name_plural = "Yazarlar"
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, default=None)
+    
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+    
+    class Meta:
         verbose_name = "Kategori"
         verbose_name_plural = "Kategoriler"
 
 
-# bu sayfa oluşturulduktan sonra admin py sayfasına tanıtılması gerekir.
-# settings.py sayfasına books uygulamamızı tanıtmamız gerekir.
-
-
-
 class Book(models.Model):
     name = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE) # yazar silindiğinde yazara ait tü kitaplar silinsin diye kullanılır.
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
-    description = models.TextField(max_length=1000, blank=True, null=True) # boş bırakmaya izin vermek için kullanılır.
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, default=None)
+    description = models.TextField(max_length=1000, blank=True, null=True)
     stock = models.IntegerField()
     price = models.FloatField()
     publication_date = models.IntegerField(blank=True, null=True)
-    created_date = models.DateTimeField(auto_now_add=True) # auto now add otomatik olarak şuanın saatini ekler.
+    created_date = models.DateTimeField(auto_now_add=True)
     activate = models.BooleanField(default=True)
-
-
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return self.name
     
-
-    class Meta: # ek bilgi eklemek için kullanılır
+    class Meta:
         verbose_name = "Kitap"
         verbose_name_plural = "Kitaplar"
--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
+from django.urls import reverse
 from django.http import HttpResponse, HttpResponseNotFound
+from .models import Author, Category, Book
 
 
 # Create your views here.
@@ -25,9 +27,13 @@ def getBooksByCategoryId(request, category_id):
     category_list = list(data.keys())
     if category_id > len(category_list):
         return HttpResponseNotFound("Yanlış kategori Seçimi...")
-    redirect_text = category_list[category_id-1]
+    category_name = category_list[category_id-1]
 
-    return redirect("/books/category/" + redirect_text)
+    redirect_url = reverse("category_name", args=[category_name])
+
+    return redirect(redirect_url)
+
+
 def getBooksByAuthor(request):
     return HttpResponse("Yazara göre kitap listesi")
 
@@ -39,14 +45,17 @@ def categories(request):
     return HttpResponse("Kategoriler Sayfası")
 
 def bookdetail(request, id):
-    return HttpResponse(f"{id} numaralı Kitap detay sayfası")
+    try:
+        book = Book.objects.get(id=id)
+        return HttpResponse(f"{book} Kitabının detay sayfası")
+    except:
+        return HttpResponseNotFound("Kitap Bulunamadı...")
 
-def bookdetailname(request, name):
-    liste = (name.split("-"))
-    name = ""
-    new = [i.title() for i in liste]
-    for n in new:
-        name+= n + " "
-    return HttpResponse(f"{name} adlı Kitap detay sayfası")
+def bookdetailname(request, slug_name):
+    try:
+        book = Book.objects.get(slug=slug_name)
+        return HttpResponse(f"{book} adlı Kitap detay sayfası")
+    except Book.DoesNotExist:
+        return HttpResponseNotFound("Kitap Bulunamadı....")
 
 
